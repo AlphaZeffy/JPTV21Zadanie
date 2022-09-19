36392ce6bbd73af4965e92e282c6e973c1b08a16
pupil <pupil@ivkhk.ee>
2022-09-19 15:12:50 +0300

    second commit

--- src/jptv21zadanie/JPTV21Zadanie.java
+++ src/jptv21zadanie/JPTV21Zadanie.java
@@ -1,21 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jptv21zadanie;
 
-/**
- *
- * @author pupil
- */
+import java.util.Scanner;
+
+
 public class JPTV21Zadanie {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("====StringEdit====");
+        System.out.println("Vvedite tekst(latinitsei): ");
+        String str = scanner.nextLine();
+        int repeat = 1;
+        do {
+            System.out.println("vi vveli sledujuwij tekst: " + str);
+            System.out.println("vvesite nomer funktsii: ");
+            System.out.println("1 : koli4estvo simvolov v tekste");
+            System.out.println("2 : loi4estvo slov v tekste");
+            System.out.println("3 : nehodit ukazannuju podstroku v tekste");
+            System.out.println("4 : zamenjaet ukazannuju podstroku na druguju podstorku");
+            int numberFunction = scanner.nextInt();
+            scanner.nextLine();
+            switch (numberFunction) {
+                case 0:
+                    System.out.println("poka");
+                    repeat = 0;
+                case 1:
+                    System.out.println("vibrana 1 func");
+                    char[] strChar = str.toCharArray();
+                    System.out.println("pos4itano: " + strChar.length);
+                    break;
+                case 2:
+                    System.out.println("vibrana 2 func");
+                    String[] wordArray = str.split(" ");
+                    System.out.println("pos4itano: " + wordArray.length + " slov");
+                    break;
+                case 3:
+                    System.out.println("vibrana 3 func");
+                    System.out.println("vvedite iskomoe");
+                    String subStr = scanner.nextLine();
+                    int indexSubStr = str.indexOf(subStr);
+                    if (indexSubStr < 0) {
+                        System.out.print("takoi podstoki net ");
+                    }else{ 
+                        indexSubStr++;
+                        System.out.println("podstroka naxoditsja na " +indexSubStr + " pozitsijax v stroke");
+                    }
+                    break;
+                case 4:
+                    
+                    System.out.println("vibrana 4 func");
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("=============================");
+            if(repeat == 0)break;
+        } while (true);
     }
     
 }
